37b04badc2e24ba6fa7ce4b0eba38c4bd7b3a94d
zette1011 <lyzette.akph@gmail.com>
2025-07-05 05:50:00 +0800

    Update compareTool.js

--- compareTool.js
+++ compareTool.js
@@ -80,26 +80,97 @@
   `;
   document.body.appendChild(modal);
 
-  window.compareContent = function() {
-    const cleanHTML = (html) => {
-      const div = document.createElement('div');
-      div.innerHTML = html;
-      // remove background colors and highlights
-      div.querySelectorAll('*').forEach(el => {
-        el.style.background = '';
-        el.style.backgroundColor = '';
-        el.removeAttribute('style');
-      });
-      return div.innerHTML.trim();
-    };
-
-    const leftHTML = cleanHTML(document.getElementById('leftEditor').innerHTML);
-    const rightHTML = cleanHTML(document.getElementById('rightEditor').innerHTML);
-
-    document.getElementById('leftResult').innerHTML = leftHTML;
-    document.getElementById('rightResult').innerHTML = rightHTML;
-
-    // You can plug in actual word-by-word diffing next here
-    // For now, it's just showing the pasted content with formatting
+  window.compareContent = function () {
+  const stripStyles = (html) => {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    div.querySelectorAll('*').forEach(el => {
+      el.style.background = '';
+      el.style.backgroundColor = '';
+      el.removeAttribute('style');
+    });
+    return div.innerHTML.trim();
   };
+
+  const leftText = stripStyles(document.getElementById('leftEditor').innerHTML);
+  const rightText = stripStyles(document.getElementById('rightEditor').innerHTML);
+
+  const dmp = new diff_match_patch();
+  const diffs = dmp.diff_main(leftText, rightText);
+  dmp.diff_cleanupSemantic(diffs);
+
+  const highlightDiff = (diffs, isRight = false) => {
+    return diffs.map(([op, data]) => {
+      if (op === 0) return data;
+      if (op === -1) return isRight ? '' : `<span class="mark-removed">${data}</span>`;
+      if (op === 1) return isRight ? `<span class="mark-added">${data}</span>` : '';
+    }).join('');
+  };
+
+  document.getElementById('leftResult').innerHTML = highlightDiff(diffs, false);
+  document.getElementById('rightResult').innerHTML = highlightDiff(diffs, true);
+};
+
+  
+// Google diff-match-patch logic (lightweight browser implementation)
+function diff_match_patch() {
+  this.diff_main = function(text1, text2) {
+    if (text1 === text2) return [[0, text1]];
+    const commonPrefixLen = this.diff_commonPrefix(text1, text2);
+    const commonPrefix = text1.substring(0, commonPrefixLen);
+    text1 = text1.substring(commonPrefixLen);
+    text2 = text2.substring(commonPrefixLen);
+
+    const commonSuffixLen = this.diff_commonSuffix(text1, text2);
+    const commonSuffix = text1.substring(text1.length - commonSuffixLen);
+    text1 = text1.substring(0, text1.length - commonSuffixLen);
+    text2 = text2.substring(0, text2.length - commonSuffixLen);
+
+    const diffs = this.diff_compute(text1, text2);
+    if (commonPrefix) diffs.unshift([0, commonPrefix]);
+    if (commonSuffix) diffs.push([0, commonSuffix]);
+    return diffs;
+  };
+
+  this.diff_compute = function(text1, text2) {
+    if (!text1) return [[1, text2]];
+    if (!text2) return [[-1, text1]];
+
+    if (text1.length > text2.length) {
+      const i = text1.indexOf(text2);
+      if (i !== -1) {
+        return [[-1, text1.substring(0, i)], [0, text2], [-1, text1.substring(i + text2.length)]];
+      }
+    } else {
+      const i = text2.indexOf(text1);
+      if (i !== -1) {
+        return [[1, text2.substring(0, i)], [0, text1], [1, text2.substring(i + text1.length)]];
+      }
+    }
+
+    return [[-1, text1], [1, text2]];
+  };
+
+  this.diff_commonPrefix = function(text1, text2) {
+    let i = 0;
+    while (i < text1.length && i < text2.length && text1[i] === text2[i]) i++;
+    return i;
+  };
+
+  this.diff_commonSuffix = function(text1, text2) {
+    let i = 0;
+    while (
+      i < text1.length &&
+      i < text2.length &&
+      text1[text1.length - i - 1] === text2[text2.length - i - 1]
+    ) i++;
+    return i;
+  };
+
+  this.diff_cleanupSemantic = function(diffs) {
+    // No-op for this lightweight version
+    return;
+  };
+}
+
 })();
